a358738fdc60518bf1f2fee67645dac6e348d563
ALuhning <a.luhning@vitalpoint.ai>
2022-09-22 15:14:39 -0400

    ipfs change

--- src/components/common/IPFSUpload/fileUpload.js
+++ src/components/common/IPFSUpload/fileUpload.js
@@ -11,7 +11,7 @@ export default function FileUpload(props) {
         register
     } = props
 
-    const ipfsApi = create('https://ipfs.infura.io:5001')
+    const ipfsApi = create('https://ceramic-node.vitalpointai.com:5001')
   
     const captureFile = (event) => {
         event.stopPropagation()
