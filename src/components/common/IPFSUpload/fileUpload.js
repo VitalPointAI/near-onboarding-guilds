45e51b52f11f0a2320237f0a5a90d8c0fcbe4a19
ALuhning <a.luhning@vitalpoint.ai>
2022-09-24 09:47:49 -0400

    test

--- src/components/common/IPFSUpload/fileUpload.js
+++ src/components/common/IPFSUpload/fileUpload.js
@@ -1,17 +1,66 @@
 import React, { useState } from 'react'
-const { create } = require('ipfs-http-client')
+const ipfsClient = require('ipfs-http-client')
+import { IPFS_CALL, TOKEN_CALL } from '../../../utils/ceramic'
+
+const axios = require('axios').default
+
+const projectId = '2FBYSWc0L9flpacZNeRChxEPcJH'
 
 export default function FileUpload(props) {
 
     const[addedFileHash, setAddedFileHash] = useState('QmZsKcVEwj9mvGfA7w7wUS1f2fLqcfzqdCnEGtdq6MBR7P')
-    
+    const[ipfsApi, setIpfsApi] = useState()
+
     const {
         handleFileHash,
         handleAvatarLoaded,
-        register
+        register, 
+        accountId
     } = props
 
-    const ipfsApi = create('https://ceramic-node.vitalpointai.com:5001')
+    useEffect(() => {
+        async function fetchIPFS(){
+            let ipfs = await createIPFS()
+            setIpfsApi(ipfs) 
+        }
+        fetchIPFS()
+        .then((res) => {
+
+        })
+    }, [])
+
+   
+    async function createIPFS() {
+        let token = await axios.post(TOKEN_CALL, 
+            {
+            accountId: accountId
+            }    
+          )
+        
+        let authToken = token.data.token
+         
+          let retrieveKey = await axios.post(IPFS_CALL, {
+            // ...data
+          },{
+            headers: {
+              'Authorization': `Basic ${authToken}`
+            }
+          })
+          console.log('projectid', projectId)
+          console.log('retrievekey', retrieveKey)
+        const auth =
+        'Basic ' + Buffer.from(projectId + ':' + retrieveKey.data.seed).toString('base64');
+    
+        const client = ipfsClient.create({
+            host: 'ipfs.infura.io',
+            port: 5001,
+            protocol: 'https',
+            headers: {
+                authorization: auth,
+            },
+        });
+        return client
+    }
   
     const captureFile = (event) => {
         event.stopPropagation()
